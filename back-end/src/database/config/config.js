c70c1eb4738d066f933676e6b053bfbbfbdebfbc
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-03-01 15:31:46 -0500

    :sparkles: [feat] sequelize-config

---
+++ back-end/src/database/config/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const config = {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DATABASE,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    dialect: 'postgres',
+    dialectOptions: {
+      dateStrings: true,
+      typeCast: true,
+    },
+    logging: false,
+    timezone: '-03:00',
+  };
+  
+  module.exports = {
+    development: config,
+    test: config,
+    production: config,
+  };
