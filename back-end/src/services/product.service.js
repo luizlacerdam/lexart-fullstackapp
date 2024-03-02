5cd224800cc5c9ee4980ba4776a6acf35cee6cd8
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-03-02 12:32:27 -0500

    :sparkles: [feat] product-service

---
+++ back-end/src/services/product.service.js
@@ -0,0 +1,34 @@
+const { product } = require('../database/models');
+
+const createProduct = async (newProduct) => {
+    const createdProduct = await product.create(newProduct);
+    return createdProduct;
+};
+
+const getAllProducts = async () => {
+    const products = await product.findAll();
+    return products;
+};
+
+const getProductById = async (id) => {
+    const productById = await product.findByPk(id);
+    return productById;
+};
+
+const deleteProductById = async (id) => {
+    const deleteProduct = await product.destroy({ where: { id } });
+    return deleteProduct;
+};
+
+const updateProduct = async (id, productData) => {
+    const updatedProduct = await product.update(productData, { where: { id } });
+    return updatedProduct;
+};
+
+module.exports = {
+    createProduct,
+    getAllProducts,
+    getProductById,
+    deleteProductById,
+    updateProduct,
+};
