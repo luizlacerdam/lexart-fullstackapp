c9ec50656bda322ebd3dddce069d0c546a97674e
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-03-02 16:45:18 -0500

    :sparkles: [feat] tokenValidation-productRouter

--- back-end/src/routers/product.router.js
+++ back-end/src/routers/product.router.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { productController } = require('../controllers');
+const { validateToken } = require('../middlewares/validateToken');
 
 const router = express.Router();
 
-router.post('/create', productController.createProduct);
-router.get('/:id', productController.getProductById);
-router.delete('/:id', productController.deleteProductById);
-router.patch('/:id', productController.updateProduct);
-router.get('/', productController.getAllProducts);
+router.post('/create', validateToken, productController.createProduct);
+router.get('/:id', validateToken, productController.getProductById);
+router.delete('/:id', validateToken, productController.deleteProductById);
+router.patch('/:id', validateToken, productController.updateProduct);
+router.get('/', validateToken, productController.getAllProducts);
 
 module.exports = router;
