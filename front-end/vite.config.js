4b81d4ff699ace3a2a50d3e6bd343ad65d2118e1
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-03-02 17:14:31 -0500

    :sparkles: [feat] hello-world-front-end

---
+++ front-end/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const defaultPort = 3000;
+const PORT = process.env.PORT || defaultPort;
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: { outDir: './build' },
+  server: { port: PORT },
+});
