e378274531aa665420c74126cecf16ba684b8d98
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-03-02 10:58:54 -0500

    :sparkles: [feat] cellphone-seeders

---
+++ back-end/src/database/seeders/20240302155800-cellphones.js
@@ -0,0 +1,54 @@
+/* eslint-disable camelcase */
+/* eslint-disable max-lines-per-function */
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert('cellphones', [
+      {
+        id: 1,
+        name: 'iPhone 13',
+        brand: 'Apple',
+        model: '13',
+        price: 7999.99,
+        color: 'Blue',
+      },
+      {
+        id: 2,
+        name: 'Galaxy S21',
+        brand: 'Samsung',
+        model: 'S21',
+        price: 4999.99,
+        color: 'Black',
+      },
+      {
+        id: 3,
+        name: 'Moto G100',
+        brand: 'Motorola',
+        model: 'G100',
+        price: 2999.99,
+        color: 'White',
+      },
+      {
+        id: 4,
+        name: 'Mi 11',
+        brand: 'Xiaomi',
+        model: '11',
+        price: 3999.99,
+        color: 'Black',
+      },
+      {
+        id: 5,
+        name: 'Zenfone 8',
+        brand: 'Asus',
+        model: '8',
+        price: 2999.99,
+        color: 'White',
+      },
+      
+    ], { underscored: true });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('cellphones', null, {});
+  },
+};
