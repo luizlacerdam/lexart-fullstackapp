61588d5ca52a52cea10f70dc29911438b7f6edc4
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-03-02 11:30:10 -0500

    :sparkles: [feat] app-user-router

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,11 +1,12 @@
-const cors =require('cors');
+const cors = require('cors');
 const express = require('express');
+const { userRouter } = require('../routers');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use('/test', (req, res) => { res.send('Hello World!'); });
-
+app.use('/user', userRouter);
 
 module.exports = app;
