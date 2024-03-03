f924d2ff634d93de7ef3d885df103b92debc6505
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-03-03 17:47:53 -0500

    :bug: [fix] db-port

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const config = {
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
+    port: process.env.POSTGRES_PORT || 5432,
     database: process.env.POSTGRES_DATABASE,
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
