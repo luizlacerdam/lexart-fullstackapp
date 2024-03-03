e4980450fdac523ea21a67e9a575d4fe5725f3fb
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-03-03 18:09:55 -0500

    :bug: [fix] db-config-model

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -12,15 +12,15 @@ const config = {
     dialectModule: pg,
     dialectOptions: {
       ssl: {
-        require: true,
+        require: false,
         rejectUnauthorized: false,
       },
-      pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-      },
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
     },
   };
   
