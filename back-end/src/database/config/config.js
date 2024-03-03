48394fcc888fac85cb62c7f26453986b6e924dee
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-03-03 17:53:00 -0500

    :bug: [fix] db-config

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -9,10 +9,21 @@ const config = {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     dialect: 'postgres',
+    protocol: 'postgres',
     dialectModule: pg,
     dialectOptions: {
       dateStrings: true,
       typeCast: true,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
     },
     logging: false,
     timezone: '-03:00',
