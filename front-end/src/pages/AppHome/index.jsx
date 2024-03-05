b93eaabc70cfc9000582e4a86d43bc7e74ebcd8b
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-03-05 16:50:42 -0500

    :sparkles: layout-file

--- front-end/src/pages/AppHome/index.jsx
+++ front-end/src/pages/AppHome/index.jsx
@@ -1,9 +1,22 @@
+import { useLoaderData } from 'react-router-dom';
 import React from 'react';
 
 export default function AppHome() {
+  const dataProducts = useLoaderData();
+  console.log(dataProducts);
   return (
-    <div>
-      <h1>App Home</h1>
+    <div className="container bg-primary d-flex flex-row justify-content-around">
+      <div className="">
+        <h1>PRODUCTS</h1>
+      </div>
+      <div>
+        <div>
+          <h1>ADD PRODUCT</h1>
+        </div>
+        <div>
+          sdasddsa
+        </div>
+      </div>
     </div>
   );
 }
